b977ae9f215ab88778c4c2bfa2e92ea1db0af94f
Alexandria Wizes <rosiea184@yahoo.co.uk>
2022-07-21 14:54:28 -0400

    chatroom

--- chatroom/main.go
+++ chatroom/main.go
@@ -1,36 +1,102 @@
+// package main
+
+// import (
+// 	"flag"
+// 	"log"
+// 	"net/http"
+// )
+
+// var addr = flag.String("addr", ":8080", "http service address")
+
+// func serveHome(w http.ResponseWriter, r *http.Request) {
+// 	log.Println(r.URL)
+// 	if r.URL.Path != "/" {
+// 		http.Error(w, "Not found", http.StatusNotFound)
+// 		return
+// 	}
+// 	if r.Method != http.MethodGet {
+// 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+// 		return
+// 	}
+// 	http.ServeFile(w, r, "home.html")
+// }
+
+// func main() {
+// 	flag.Parse()
+// 	hub := newHub()
+// 	go hub.run()
+// 	http.HandleFunc("/", serveHome)
+// 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+// 		serveWs(hub, w, r)
+// 	})
+// 	err := http.ListenAndServe(*addr, nil)
+// 	if err != nil {
+// 		log.Fatal("ListenAndServe: ", err)
+// 	}
+// }
+
 package main
 
 import (
-	"flag"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-var addr = flag.String("addr", ":8080", "http service address")
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
 
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.ServeFile(w, r, "home.html")
+var upgrader = websocket.Upgrader{}
+
+type Message struct {
+	Message string `json:"message"`
 }
 
-func main() {
-	flag.Parse()
-	hub := newHub()
-	go hub.run()
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		serveWs(hub, w, r)
-	})
-	err := http.ListenAndServe(*addr, nil)
+func HandleClients(w http.ResponseWriter, r *http.Request) {
+	go broadcastMessagesToClients()
+
+	websocket, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		log.Fatal("error upgrading GET request to a websocket :: ", err)
+	}
+	defer websocket.Close()
+	clients[websocket] = true
+	for {
+		var message Message
+
+		err := websocket.ReadJSON(&message)
+		if err != nil {
+			log.Printf("error occurred while reading message : %v", err)
+			delete(clients, websocket)
+			break
+		}
+		broadcast <- message
+	}
+}
+func broadcastMessagesToClients() {
+	for {
+		message := <-broadcast
+		for client := range clients {
+			err := client.WriteJSON(message)
+			if err != nil {
+				log.Printf("error occurred while writing message to client: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+	http.HandleFunc("/echo", HandleClients)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("error starting http server :: ", err)
+
+		return
 	}
 }
